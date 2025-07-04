db00795bf112a16fb8e5767184e18681a4dd094f
Temitayo Kayode <temitayokayode5@gmail.com>
2025-07-04 15:55:50 +0100

    Update RoomDetails.tsx

--- src/components/RoomDetails.tsx
+++ src/components/RoomDetails.tsx
@@ -6,8 +6,8 @@ const RoomDetails: React.FC = () => {
   const { rooms } = useBooking();
 
   const roomImages = {
-    VIP: 'public/mars1.png',
-    Regular: 'public/mars.png'
+    VIP: '/public/mars1.png',
+    Regular: '/public/mars.png'
   };
 
   const amenities = [
